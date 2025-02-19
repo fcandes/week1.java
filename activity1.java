dd3d6891339f21fcd1d943a4d965833a518fc5d0
fcandes <fcandes@paterostechnologicalcollege.edu.ph>
2025-02-19 21:02:57 +0800

    Add files via upload
    added name; Francis C. Andes

---
+++ activity1.java
@@ -0,0 +1,20 @@
+public class StudentNames {
+    public static void main(String[] args) {
+        System.out.println("List of Students:");
+
+<<<<<<< HEAD
+        // System.out.println("Mykah Gabejan - BSIT 2M");
+=======
+        // Students, add your names below using the format:
+        // System.out.println("Your Name - SECTION");
+        // Example:
+        // System.out.println("Juan Dela Cruz - BSIT 2M");
+        System.out.println("Xander Olimberio - BSIT 2M");
+        System.out.println("Stephanie Capuno - BSCS 2M");
+        System.out.println("Clarence P. Cardenas - BSIT 2M");
+        System.out.println("James Michael B. Dela Cruz - BSIT 2M");
+      System.out.println("Mykah Gabejan - BSIT 2M");
+      System.out.println("Francis C. Andes - BSIT 2M");
+
+    }
+}
